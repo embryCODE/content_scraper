25200e968da734b53c300e70bd2eddf162c7d66a
Mason Embry <mason@embrycode.com>
2016-10-19 16:34:49 -0400

    Starting point.

--- scraper.js
+++ scraper.js
@@ -1,20 +1,7 @@
-var fs = require('fs');
+(function() {
 
-// Create directory 'data' if it doesn't already exist.
-function createDir(dirName) {
-  try {
-    fs.statSync(dirName);
-  } catch(e) {
-    fs.mkdirSync(dirName);
-  }
-}
-createDir('data');
+    'use_strict';
 
-// Create .csv file named after current date.
-var currentDate = new Date();
-var day = currentDate.getDate();
-var month = currentDate.getMonth() + 1;
-var year = currentDate.getFullYear();
-dateToday = year + "-" + month + "-" + day;
+    
 
-fs.writeFile(dateToday + '.csv');
+})();
