d2e890df5e8763d265e4d8ae4d28e0a51601d3e3
Mason Embry <mason@embrycode.com>
2016-10-20 16:11:35 -0500

    Updated comments.

--- scraper.js
+++ scraper.js
@@ -1,84 +1,88 @@
 (function() {
 
-    'use_strict';
+  'use_strict';
 
-    // Modules
-    var fs = require('fs'),
-        Xray = require('x-ray'),
-        xray = new Xray(),
-        json2csv = require('json2csv');
+  // Modules
+  var fs = require('fs'),
+    Xray = require('x-ray'),
+    xray = new Xray(),
+    json2csv = require('json2csv');
 
-    // Create directory 'data' if it doesn't already exist.
-    function createDir(dirName) {
-        try {
-            fs.statSync(dirName);
-        } catch (e) {
-            fs.mkdirSync(dirName);
-        }
+  // Create directory 'data' if it doesn't already exist.
+  function createDir(dirName) {
+    try {
+      fs.statSync(dirName);
+    } catch (e) {
+      fs.mkdirSync(dirName);
     }
-    createDir('data');
+  }
+  createDir('data');
 
-    // Create .csv file named after current date.
-    var currentDate = new Date();
-    var day = currentDate.getDate();
-    var month = currentDate.getMonth() + 1;
-    var year = currentDate.getFullYear();
-    csvFileName = year + "-" + month + "-" + day;
+  /* Log error. Take date and error message as argument.
+  Create scraper-error.log if it doesn't exist
+  Append error message to scraper-error.log. */
+  function logError(date, error) {
+    var errorText = 'Date: ';
+    errorText += date + '\n';
+    errorText += error + '\n';
+    errorText += '\n';
 
-    // Log error.
-    function logError(date, error) {
-        var errorText = 'Date: ';
-        errorText += date + '\n';
-        errorText += error + '\n';
-        errorText += '\n';
+    console.log(errorText);
+  }
 
-        console.log(errorText);
-    }
+  /* Use x-ray to dig down into appropriate links and pull
+  Title, Price, ImageURL, and URL from page. */
+  xray('http://www.shirts4mike.com/',
+    xray('.shirts a@href',
+      xray('.products li', [{
+        Title: 'a img@alt',
+        Price: xray('a@href', '.shirt-details h1 span'),
+        ImageURL: xray('a@href', '.shirt-picture img@src'),
+        URL: 'a@href'
+      }])
+    )
+  )(function(error, data) {
+    var resultsJSON = data;
 
-    var resultsJSON;
+    // Create timestamp of when scrape is completed.
+    var date = new Date();
 
-    xray('http://www.shirts4mike.com/',
-        xray('.shirts a@href',
-            xray('.products li', [{
-                Title: 'a img@alt',
-                Price: xray('a@href', '.shirt-details h1 span'),
-                ImageURL: xray('a@href', '.shirt-picture img@src'),
-                URL: 'a@href'
-            }])
-        )
-    )(function(error, data) {
+    /* Create resultsCSV from resultsJSON using json2csv then
+    create and save CSV file in data directory.
+    If error, call logError(). */
+    if (!error) {
 
-        // Create timestamp of when scrape is completed.
-        var date = new Date();
-
-        if (!error) {
-            resultsJSON = data;
-
-            try {
-                resultsCSV = json2csv({
-                    data: resultsJSON,
-                    fields: [
-                        'Title',
-                        'Price',
-                        'ImageURL',
-                        'URL', {
-                            label: 'Time',
-                            default: date
-                        }
-                    ]
-                });
-            } catch (err) {
-                console.error(err);
+      try {
+        resultsCSV = json2csv({
+          data: resultsJSON,
+          fields: [
+            'Title',
+            'Price',
+            'ImageURL',
+            'URL', {
+              label: 'Time',
+              default: date
             }
+          ]
+        });
+      } catch (err) {
+        logError(date, err);
+      }
 
-            fs.writeFile('data/' + csvFileName + '.csv', resultsCSV, function(err) {
-                if (err) {
-                    logError(date, err);
-                }
-            });
-        } else {
-            logError(date, error);
+      // Set CSV file name with yyyy-mm-dd.
+      csvFileName = date.getFullYear() +
+        "-" + date.getMonth() +
+        "-" + date.getDate();
+
+      // Write CSV file to data directory.
+      fs.writeFile('data/' + csvFileName + '.csv', resultsCSV, function(err) {
+        if (err) {
+          logError(date, err);
         }
-    });
+      });
+    } else {
+      logError(date, error);
+    }
+  });
 
 })();
