26513eb97da969d9964da3a4c16a88f8a3507d91
Mason Embry <mason@embrycode.com>
2016-10-21 15:14:20 -0500

    Finished JSDoc comments.

--- scraper.js
+++ scraper.js
@@ -1,10 +1,15 @@
+'use_strict';
+
 /**
  * @file Portfolio Project 6 for Techdegree. Build a Content Scraper.
  * {@link https://teamtreehouse.com/projects/build-a-content-scraper}
  * @author Mason Embry <mason@embrycode.com>
+ *
+ * @requires fs
+ * @requires x-ray
+ * @requires json2csv
+ *
  */
-
-'use_strict';
 
 // Modules
 var fs = require('fs'),
@@ -12,7 +17,10 @@ var fs = require('fs'),
   xray = new Xray(),
   json2csv = require('json2csv');
 
-// Create directory 'data' if it doesn't already exist.
+/**
+  * Create directory 'data' if it doesn't already exist.
+  * @param {string} dirName Name of directory to create.
+  */
 function createDir(dirName) {
   try {
     fs.statSync(dirName);
@@ -23,15 +31,14 @@ function createDir(dirName) {
 createDir('data');
 
 /**
- * Log error. Take date and error message as argument.
  * Create scraper-error.log if it doesn't exist.
  * Append error message to scraper-error.log.
- * @param {object} date Date object.
+ * @param {object} timestamp Date object.
  * @param {string} error Error message.
  */
-function logError(date, error) {
+function logError(timestamp, error) {
   var errorText = 'Date: ';
-  errorText += date + '\n';
+  errorText += timestamp + '\n';
   errorText += error + '\n';
   errorText += '\n';
   fs.appendFile('scraper-error.log', errorText, 'utf8', function(err) {
@@ -42,8 +49,10 @@ function logError(date, error) {
   });
 }
 
-/* Use x-ray to dig down into appropriate links and pull
-Title, Price, ImageURL, and URL from page. */
+/**
+  * Use x-ray to dig down into appropriate links and pull
+  * Title, Price, ImageURL, and URL from page.
+  */
 xray('http://www.shirts4mike.com/',
   xray('.shirts a@href',
     xray('.products li', [{
@@ -57,11 +66,13 @@ xray('http://www.shirts4mike.com/',
   var resultsJSON = data;
 
   // Create timestamp when scrape is completed.
-  var date = new Date();
+  var timestamp = new Date();
 
-  /* Create resultsCSV from resultsJSON using json2csv then
-  create and save CSV file in data directory.
-  If error, call logError(). */
+  /**
+   * Create resultsCSV from resultsJSON using json2csv then
+   * create and save CSV file in data directory.
+   * If error, call logError().
+   */
   if (!error) {
     var resultsCSV;
 
@@ -74,26 +85,28 @@ xray('http://www.shirts4mike.com/',
           'ImageURL',
           'URL', {
             label: 'Time',
-            default: date.toString()
+            default: timestamp.toString()
           }
         ]
       });
     } catch (err) {
-      logError(date, err);
+      logError(timestamp, err);
     }
 
     // Set CSV file name with yyyy-mm-dd.
-    var csvFileName = date.getFullYear() +
-      '-' + date.getMonth() +
-      '-' + date.getDate();
+    var csvFileName = timestamp.getFullYear() +
+      '-' + timestamp.getMonth() +
+      '-' + timestamp.getDate();
 
-    // Write CSV file to data directory.
+    /**
+     * Write CSV file to data directory.
+     */
     fs.writeFile('data/' + csvFileName + '.csv', resultsCSV, function(err) {
       if (err) {
-        logError(date, err);
+        logError(timestamp, err);
       }
     });
   } else {
-    logError(date, error);
+    logError(timestamp, error);
   }
 });
